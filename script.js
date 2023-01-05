4778dfa1ba07fc0eeeb76fed74ca14d5cd8ef771
CarlosZ123 <118822229+CarlosZ123@users.noreply.github.com>
2023-01-05 11:05:00 -0600

    update with base for final script

---
+++ script.js
@@ -0,0 +1,42 @@
+
+//Here are some guiding questions:
+
+//1 What event listeners do you need to start off with?
+    // - add bookmark
+    // - remove bookmark
+
+//2 Can you correctly add bookmarks?
+//3 What UI element allows a user to remove a previously added bookmark?
+//4 How can you make sure that the correct bookmark is removed?
+
+
+
+const = bookmarks [];
+
+const removeBookmark = document.getElementById();
+favoritesMarkDel.addEventListener('click', )
+// set variable for bookmarks list in element in html
+// add event listener for clicking delete of bookmark , push new bookmarks array with changes
+// show/render updated favorites list
+
+const addBookmarkButton = document.getElementById();
+addBookmarkButton.addEventListener('click', ()=> {
+    // get bookmark name
+
+    //get bookmark address
+
+    //push object values {name, address}
+
+    // show/render updated favorites list
+});
+
+const renderMarks = () => {
+    const bookmarkList = document.getElementById();
+    bookmarks.map()
+
+    bookmarkList.appendChild();
+}
+// set variable for bookmarks list element in html
+// map bookmarks array with new html list item for bookmark name and address
+// pass to html with innerhtml
+// append to bookmarklist
